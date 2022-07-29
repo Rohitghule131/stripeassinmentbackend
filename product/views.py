a6369c2e9542085c89c79f666bb45f1e2a3547be
Rohit Ghule <rohit.ghule@mindbowser.com>
2022-07-29 09:28:04 +0530

    removed bugs, add validation minor changes

--- product/views.py
+++ product/views.py
@@ -2,10 +2,6 @@ from rest_framework import generics
 from rest_framework.response import Response
 from product.models import Products
 from product.serializer import ProductSerializer
-import json
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
 
 class GetProducts(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
@@ -18,5 +14,3 @@ class GetProducts(generics.RetrieveAPIView):
         obj = self.get_queryset()
         serializer = self.get_serializer(obj,many=True)
         return Response(serializer.data)
-
-#gutsy-fiery-brave-lead
